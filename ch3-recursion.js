e299bf264089304d0755be03add1be344cfe49f6
Jonathan Pinlac <jonppinlac@gmail.com>
2016-02-17 09:47:17 -0500

    Fixed recursion problem to work with negative values"

--- ch3-recursion.js
+++ ch3-recursion.js
@@ -3,8 +3,11 @@ function evenOrOdd(number) {
 		return 'even';
 	else if(number === 1)
 		return 'odd';
-	else
+	else{
+		if(number<0)
+			return evenOrOdd(number+2);
 		return evenOrOdd(number-2);
+	}
 }
 console.log(evenOrOdd(50));
 console.log(evenOrOdd(75));
