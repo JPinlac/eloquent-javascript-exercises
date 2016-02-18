4f578fe6379f1fbc2d30903a59ba62cba317a64a
Jonathan Pinlac <jonppinlac@gmail.com>
2016-02-18 14:00:00 -0500

    Supports negative step values

--- ch4-sum-range.js
+++ ch4-sum-range.js
@@ -2,17 +2,31 @@
  * Created by Jonathan Pinlac on 2/18/2016.
  */
 'use strict';
-function range(start, end) {
+function range(start, end, step) {
     let array = [];
-    for(let x = start; x <= end; x++){
-        array.push(x);
+    if(step === undefined){
+        if(start < end)
+            step = 1;
+        else {
+            step = -1;
+        }
+    }
+    if(step > 0) {
+        for (let x = start; x <= end; x += step) {
+            array.push(x);
+        }
+    }
+    else {
+        for (let x = start; x >= end; x += step) {
+            array.push(x);
+        }
     }
     return array;
 }
 function sum(array) {
     let total = 0;
-    for(let item in array) {
-        total += array[item];
+    for(let x = 0; x < array.length; x++) {
+        total += array[x];
     }
     return total;
 }
